0793b5243a7df1a4862417f984c9ec1328692256
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2022-11-19 15:33:36 -0300

    component ok

---
+++ frontend/src/components/ProductCard/index.js
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import CartContext from '../../context/CartContext'
+import { MdAddCircle, MdRemoveCircle } from 'react-icons/md'
+
+const ProductCard = ({ item }) => {
+  const context = useContext(CartContext)
+  const [qty, setQty] = useState(1)
+  const { imageUrl, name, stock, price, _id } = item
+
+  const reducir = () => {
+    if (qty >= 2) {
+      setQty(qty - 1)
+    }
+  }
+
+  const aumentar = () => {
+    if (qty < stock) {
+      setQty(qty + 1)
+    }
+  }
+
+  return (
+    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <div class="card">
+        <img src={imageUrl} class="card-img-top" alt="" />
+        <div class="card-body d-flex flex-column align-items-center">
+          <Link to={`/producto/${_id}`}>
+            <h5 class="card-title">{name}</h5>
+          </Link>
+          <p class="card-text">${price}</p>
+          {stock ? (
+            <div className="d-flex align-items-center qty-container">
+              <button onClick={reducir} disabled={qty === 1}>
+                <MdRemoveCircle />
+              </button>
+              <span className="my-2 text-center">{qty}</span>
+              <button onClick={aumentar} disabled={qty === stock}>
+                <MdAddCircle />
+              </button>
+            </div>
+          ) : (
+            <div className="d-flex align-items-center qty-container">
+              <p className="text-danger fw-semibold">Sin stock</p>
+            </div>
+          )}
+
+          <button
+            onClick={() => context.addToCart(item, qty)}
+            disabled={!(stock > 0)}
+            className="btn btn-primary btn-sm btn-dark"
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
